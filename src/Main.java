ee9a29c73456b2939a2dbd4dc5c5d3d5fc787ce3
lenkamanova <lenka.manova@volny.cz>
2025-06-05 13:30:27 +0200

    Přidán rodinný dům

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+       // Random nahCislo = new Random();
+
+        RodinnyDum dumNovaku = new RodinnyDum();
+        dumNovaku.pocetPoschodi = 4;
+        dumNovaku.rozloha = 457f;
+        dumNovaku.material = "mramor";
+
+        RodinnyDum dumZajicu = new RodinnyDum();
+        dumZajicu.pocetPoschodi = 3;
+        dumZajicu.rozloha = 6468f;
+
+        
+
+        Random genCisel = new Random();
     }
 }
---
+++ src/RodinnyDum.java
@@ -0,0 +1,13 @@
+public class RodinnyDum {
+    int pocetPoschodi;
+    String material;
+    float rozloha;
+    int pocetOken;
+
+    public void vypocetCenyDomu (){
+        System.out.println(rozloha * 8000);
+
+    }
+
+
+}
